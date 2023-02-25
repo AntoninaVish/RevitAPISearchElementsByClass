9dea4a40ebd6b11fd90733ff617ab8327055db48
AntoninaVish <vishivtonya@gmail.com>
2023-02-25 16:59:07 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPISearchElementsByClass
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            //создаем новую переменную список стен
+            //ищем стены по всей модели
+            var walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>() //приводим к определенному типу
+                .ToList();//преобрзуем в список стен
+
+            TaskDialog.Show("Wall info", walls.Count.ToString());//выводим количество стен
+
+            return Result.Succeeded;
+        }
+    }
+}
